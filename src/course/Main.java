5b964a96fd63be8407240bebb70ae262bdaddf00
sergio-macedo <sergio.garoma@hotmail.com>
2022-05-31 15:52:24 +0200

    instantieted objects

--- src/course/Main.java
+++ src/course/Main.java
@@ -1,13 +1,29 @@
 package course;
 
-import entities.Order;
-import enums.OrderStatus;
+import entities.Department;
+import entities.Worker;
+import enums.WorkerLevel;
 
 import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter department's name");
+        String departmentName = sc.nextLine();
+        System.out.print("Enter worker data");
+        String workerName =sc.nextLine();
+        System.out.print("Level");
+        String workerLevel =sc.nextLine();
+        System.out.print("Base Salary");
+        double baseSalary =sc.nextDouble();
+        Worker worker = new Worker(workerName, WorkerLevel.valueOf(workerLevel),baseSalary, new Department(departmentName));
+
 
     }
 }
