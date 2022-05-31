85e2258c683ec6640627dd5172c25ac092a80942
sergio-macedo <sergio.garoma@hotmail.com>
2022-05-31 13:34:51 +0200

    starting the enum project

---
+++ src/course/Main.java
@@ -0,0 +1,22 @@
+package course;
+
+import entities.Order;
+import enums.OrderStatus;
+
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Order order = new Order(1080,new Date(), OrderStatus.PENDING_PAYMENT);
+
+
+        OrderStatus os1 = OrderStatus.DELIVERED;
+
+        OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+        System.out.println(os1);
+        System.out.println(os2);
+
+    }
+}
