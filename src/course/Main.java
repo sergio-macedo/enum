3f1672e37d22e6d035450a7f62521393634e9220
sergio-macedo <sergio.garoma@hotmail.com>
2022-05-31 14:59:23 +0200

    entities enums and main from the enum project

--- src/course/Main.java
+++ src/course/Main.java
@@ -9,14 +9,5 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Order order = new Order(1080,new Date(), OrderStatus.PENDING_PAYMENT);
-
-
-        OrderStatus os1 = OrderStatus.DELIVERED;
-
-        OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
-        System.out.println(os1);
-        System.out.println(os2);
-
     }
 }
